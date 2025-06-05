bace146baaadc41df5ef56438c2091102de241eb
ozairFilho <ozair.filho@escola.pr.gov.br>
2025-06-05 09:44:26 -0300

    rht

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
 document.addEventListener('DOMContentLoaded', function (){
     const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade')
-    const botaoDeAcessibilidade = document.getElementById('opcao-acessibilidade')
+    const botaoDeAcessibilidade = document.getElementById('opcoes-acessibilidade')
 
 botaoDeAcessibilidade.addEventListener('click', function () {
 botaoDeAcessibilidade.classList.toggle('rotacao-boto');
 botaoDeAcessibilidade.classList.toggle('apresenta-lista')
     })
+
+    const aumentaFontaBotao = document.getElementById ('aumentar-fonte');
+    const diminuiFonteBotao = document.getElementById ('diminuir-fonte');
+
+    const alternaContraste = document.getElementById ('alterna-contraste');
+
+    let tamanhoAtualFonte = 1;
+
+    aumentaFonteBotao.addEventListener('click', function(){
+        tamanhoAtualFonte +=0.1;
+        document.body.style.fontsize = `${tamanhoAtualFonte}rem`
+    })
+
+     diminuiFonteBotao.addEventListener('click', function () {
+        tamanhoAtualFonte -= 0.1;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+    })
+
+    alternaContraste.addEventListener('click', function () {
+        document.body.classList.toggle('alto-contraste')
+    })
+})
